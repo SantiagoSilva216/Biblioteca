6312c2d19982de81a7e1a18f36e0d9d28e8509f8
Brayan Santiago Cardoso <brayan_cardososi@fet.edu.co>
2025-09-24 21:17:27 -0500

    ejemplo de biblioteca

--- src/main/java/Biblioteca/Main.java
+++ src/main/java/Biblioteca/Main.java
@@ -3,14 +3,12 @@ package Biblioteca;
 public class Main {
     public static void main(String[] args) {
 
-        // Arreglo polimórfico de tipo MaterialBiblioteca
         MaterialBiblioteca[] materiales = new MaterialBiblioteca[3];
 
         materiales[0] = new Libro("El Quijote", "Miguel de Cervantes", 1605, "Novela");
         materiales[1] = new Revista("National Geographic", "Varios", 2024, 210);
         materiales[2] = new Tesis("Inteligencia Artificial", "Ana Pérez", 2023, "Universidad Nacional");
 
-        // Polimorfismo: cada objeto ejecuta su propia versión de mostrarInfo()
         for (MaterialBiblioteca material : materiales) {
             material.mostrarInfo();
         }
